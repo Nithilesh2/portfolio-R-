1484bd2cd51a8bd0ae6d1ab261ff745dbb606bda
nithilesh <sunnybnithilesh@gmail.com>
2024-08-03 23:35:03 +0530

    home and logon Page

---
+++ src/components/Home.jsx
@@ -0,0 +1,183 @@
+import React, { useContext } from "react"
+import styles from "../App.module.css"
+import CustomCursor from "./CustomCursor"
+import {
+  IoCloseOutline,
+  IoPerson,
+  IoRemoveOutline,
+  IoSquareOutline,
+} from "react-icons/io5"
+import TaskBar from "./TaskBar"
+import AppContext from "../context/AppContext"
+import UiSkills from "./UiSkills"
+
+const Home = () => {
+  const {
+    resumeDoubleClick,
+    closeResume,
+    screenSizeResume,
+    setScreenSizeResume,
+    setCloseResume,
+    fileIconUrl,
+    resumeDownload,
+    setMinimizeResume,
+    minimizeResume,
+    minimizeSkills,
+    closeSkills,
+    setCloseSkills,
+    setMinimizeSkills,
+    screenSizeSkills,
+    setScreenSizeSkills,
+    skillsDoubleClick,
+    name,
+  } = useContext(AppContext)
+  return (
+    <>
+      <CustomCursor />
+      <div className={styles.desktop}>
+        <div className={styles.leftOne}>
+          <div
+            className={styles.resumeFolder}
+            onDoubleClick={resumeDoubleClick}
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3767/3767084.png"
+              alt="resume"
+            />
+            <div className={styles.resume}>My Resume</div>
+          </div>
+          {minimizeResume ? (
+            ""
+          ) : (
+            <div
+              className={
+                closeResume
+                  ? styles.myResumeBoxDeActive
+                  : screenSizeResume
+                  ? styles.myResumeBoxActiveBig
+                  : styles.myResumeBoxActiveSmall
+              }
+            >
+              <div className={styles.myResumeTop}>
+                <div className={styles.myResumeTopLeft}>
+                  <IoPerson className={styles.myResumeTopLeftUser} />
+                  <p>Nithilesh's Resume</p>
+                </div>
+                <div className={styles.myResumeTopRight}>
+                  <button
+                    className={styles.myResumeTopRightButton}
+                    onClick={() => {
+                      setMinimizeResume(!minimizeResume)
+                    }}
+                  >
+                    <IoRemoveOutline />
+                  </button>
+                  <button
+                    className={styles.myResumeTopRightButton}
+                    onClick={() => setScreenSizeResume(!screenSizeResume)}
+                  >
+                    <IoSquareOutline />
+                  </button>
+                  <button
+                    className={styles.myResumeTopRightButton}
+                    onClick={() => {
+                      setCloseResume(true)
+                    }}
+                  >
+                    <IoCloseOutline />
+                  </button>
+                </div>
+              </div>
+              <div className={styles.myResumeCenter}>
+                <div className={styles.resumeDownload}>
+                  <img
+                    src={fileIconUrl}
+                    alt="i"
+                    title="Click to download"
+                    onClick={resumeDownload}
+                  />
+                  <div>Resume</div>
+                </div>
+              </div>
+              <div className={styles.myResumeBottom}>
+                <p>1 Object(s)</p>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className={styles.leftTwo}>
+          <div
+            className={styles.resumeFolder}
+            onDoubleClick={skillsDoubleClick}
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3767/3767084.png"
+              alt="resume"
+            />
+            <div className={styles.resume}>Skills</div>
+          </div>
+
+          {minimizeSkills ? (
+            ""
+          ) : (
+            <div
+              className={
+                closeSkills
+                  ? styles.myResumeBoxDeActive
+                  : screenSizeSkills
+                  ? styles.myResumeBoxActiveBig
+                  : styles.myResumeBoxActiveSmall
+              }
+            >
+              <div className={styles.myResumeTop}>
+                <div className={styles.myResumeTopLeft}>
+                  <IoPerson className={styles.myResumeTopLeftUser} />
+                  <p>Skills</p>
+                </div>
+                <div className={styles.myResumeTopRight}>
+                  <button
+                    className={styles.myResumeTopRightButton}
+                    onClick={() => {
+                      setMinimizeSkills(!minimizeSkills)
+                    }}
+                  >
+                    <IoRemoveOutline />
+                  </button>
+                  <button
+                    className={styles.myResumeTopRightButton}
+                    onClick={() => setScreenSizeSkills(!screenSizeSkills)}
+                  >
+                    <IoSquareOutline />
+                  </button>
+                  <button
+                    className={styles.myResumeTopRightButton}
+                    onClick={() => {
+                      setCloseSkills(true)
+                    }}
+                  >
+                    <IoCloseOutline />
+                  </button>
+                </div>
+              </div>
+              <div className={styles.myResumeCenter}>
+                <div className={styles.skillsBox}>
+                  <h2>Skills</h2>
+                  <UiSkills />
+                </div>
+              </div>
+              <div className={styles.myResumeBottom}>
+                <p>1 Object(s)</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      {name.map((data) => (
+        <TaskBar key="index" source={fileIconUrl} />
+      ))}
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/LogonPage.jsx
@@ -0,0 +1,70 @@
+import React, { useRef } from "react"
+import styles from "../styles/LogonPage.module.css"
+import { useNavigate } from "react-router-dom"
+
+const LogonPage = () => {
+  const userNameRef = useRef(null)
+  const passwordRef = useRef(null)
+
+  const navigate = useNavigate()
+
+  const okBtnClicked = () => {
+    if (userNameRef.current.value === "admin") {
+      if (passwordRef.current.value === "123") {
+        navigate("/main")
+      }
+      alert("Wrong password...")
+    }
+    if (userNameRef.current.value !== "admin") {
+      alert("Wrong user name...")
+    }
+  }
+
+  return (
+    <>
+      <div className={styles.logonPage}>
+        <div className={styles.logonBox}>
+          <div className={styles.logonBoxTop}>Enter Password</div>
+          <div className={styles.logonBoxCenter}>
+            <img
+              src="https://cdn2.steamgriddb.com/logo/72750ee512b0ef9c77aa7e84765a8173.png"
+              alt="windows nt 5.0"
+              className={styles.LogonBoxCenterIcon}
+            />
+          </div>
+          <div className={styles.logonBoxBottom}>
+            <div className={styles.logonBoxBottomUserNameBox}>
+              <label className={styles.logonBoxBottomUserName}>
+                User name :
+              </label>
+              <input
+                type="text"
+                className={styles.userName}
+                ref={userNameRef}
+              />
+            </div>
+            <div className={styles.logonBoxBottomPasswordBox}>
+              <label className={styles.logonBoxBottomPassword}>
+                Password :
+              </label>
+              <input
+                type="password"
+                className={styles.password}
+                ref={passwordRef}
+              />
+            </div>
+          </div>
+          <div className={styles.bottomDown}>
+            <button className={styles.bottomDownOk} onClick={okBtnClicked}>
+              Ok
+            </button>
+            <button className={styles.bottomDownCancel}>Cancel</button>
+            <button className={styles.bottomDownShutdown}>Shutdown...</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LogonPage
